52117e397e0ca109620e4a9fe46c2c68f13fc97c
Daniel PC <raresgeo9@gmail.com>
2024-07-02 14:15:13 +0300

    feat: more weak prng

--- app/src/Father.sol
+++ app/src/Father.sol
@@ -16,9 +16,32 @@ contract Father is Inherited, GrandFather {
     uint256 public fatherNumber;
 
     constructor(uint _fatherNumber) {
+        uint finalResult = _fatherNumber + 1;
         if (block.timestamp % 2 == 0) {
-            fatherNumber = _fatherNumber;
+            finalResult += 1;
         }
         fatherNumber = _fatherNumber * 3;
+        if (block.timestamp % 3 == 0) {
+            finalResult += 1;
+        }
+        if (block.timestamp % 5 == 0) {
+            finalResult += 1;
+        }
+        if (block.timestamp % 7 == 0) {
+            finalResult += 1;
+        }
+        if (block.timestamp % 11 == 0) {
+            finalResult += 1;
+        }
+        if (block.timestamp % 13 == 0) {
+            finalResult += 1;
+        }
+        if (block.timestamp % 17 == 0) {
+            finalResult += 1;
+        }
+        if (block.timestamp % 19 == 0) {
+            finalResult += 1;
+        }
+        setGrandFatherNumber(finalResult);
     }
 }
