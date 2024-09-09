2e797ef8beca7b8dda74334cd18b5357a93998c2
Daniel PC <raresgeo9@gmail.com>
2024-09-09 12:33:18 +0300

    feat: a new contract with constructor argumnets

---
+++ app/src/LessSimpleContract.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+contract SimpleContract {
+    uint256 public randomNumber;
+
+    constructor(uint _constructorNumber) {
+        randomNumber = _constructorNumber;
+    }
+
+    function setNumber(uint256 _number) public {
+        randomNumber = _number;
+    }
+
+    function getNumber() public view returns (uint256) {
+        return randomNumber;
+    }
+}
