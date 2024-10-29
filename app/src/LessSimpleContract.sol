f14fb63f60c1040ca5eb81e959b5d483b648a799
Daniel PC <raresgeo9@gmail.com>
2024-10-29 19:54:21 +0200

    feat: more changes haha hello

--- app/src/LessSimpleContract.sol
+++ app/src/LessSimpleContract.sol
@@ -8,7 +8,12 @@ contract LessSimpleContract {
     }
 
     function setNumber(uint256 _number) public {
-        randomNumber = _number + (13 * (_number % 169));
+        randomNumber =
+            _number +
+            uint256(
+                int256(13 * (_number % 169)) *
+                    (block.timestamp % 2 == 0 ? int256(-1) : int256(1))
+            );
     }
 
     function getNumber() public view returns (uint256) {
